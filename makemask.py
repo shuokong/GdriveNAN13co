41f67ee2754cde8e9b613cf6a72beae36a0271f0
Kong Shuo <skong.astro@gmail.com>
2020-02-04 22:55:39 -0700

    make mask

--- makemask.py
+++ makemask.py
@@ -4,12 +4,19 @@ import pyregion
 from astropy.io import fits
 import numpy as np
 
-region_name = "ds9.reg"
+region_name = "F6polygon.reg"
 r = pyregion.open(region_name)
 
-hdu1 = fits.open('nofreq_mom1_SE_mask_13co_pix_2_Tmb.fits')[0]
-mymask = r.get_mask(hdu=hdu1)
-hdu1.data[~mymask] = 0
-hdu1.data[mymask] = 1
-fits.writeto('SE_filaments.fits', hdu1.data, hdu1.header, clobber=True)
+temphdu = fits.open('peak_SE_mask_13co_pix_2_Tmb.fits')[0]
+temphdu.data = temphdu.data[0,:,:]
+temphdu.header['NAXIS'] = 2
+del temphdu.header['CRPIX3']
+del temphdu.header['CDELT3']
+del temphdu.header['CRVAL3']
+del temphdu.header['CTYPE3']
+#del temphdu.header['NAXIS3']
+mymask = r.get_mask(hdu=temphdu)
+temphdu.data[mymask] = 1
+temphdu.data[~mymask] = 0
+fits.writeto('F6polygon.fits', temphdu.data, temphdu.header, clobber=True)
 
